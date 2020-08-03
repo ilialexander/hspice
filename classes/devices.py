0b2751ba4a62a910cad55779ff7291f498437b0f
iliabautista <iliabautista@mail.usf.edu>
2020-08-03 17:30:39 -0400

    created spice files from inverter_script, need some edits

---
+++ classes/devices.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class Devices:
+
+    def __init__(self):
+
+
+    '''Passive Devices'''
+
+    def resistor(self):
+
+    def capacitor(self):
+
+    def inductor(self):
+
+    '''FETs'''
